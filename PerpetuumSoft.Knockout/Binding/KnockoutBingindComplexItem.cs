527568aa335b995565de6a16a31623c72eab35ee
Nigel Whatling <nigelw@n-vision.com.au>
2013-09-07 00:17:25 +1000

    Fix typo in KnockoutBindingComplexItem class name.

--- PerpetuumSoft.Knockout/Binding/KnockoutBingindComplexItem.cs
+++ PerpetuumSoft.Knockout/Binding/KnockoutBingindComplexItem.cs
@@ -1,37 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace PerpetuumSoft.Knockout
 {
-  public class KnockoutBingindComplexItem : KnockoutBindingItem
+    [Obsolete("This class has a typo and should one day be removed. Use KnockoutBindingComplexItem instead.", false)]
+    public class KnockoutBingindComplexItem : KnockoutBindingComplexItem
   {
-    private readonly List<KnockoutBindingItem> subItems = new List<KnockoutBindingItem>();
-
-    public void Add(KnockoutBindingItem subItem)
-    {
-      subItems.Add(subItem);
-    }
-
-    public override bool IsValid()
-    {
-      return subItems.Count > 0;
-    }
-
-    public override string GetKnockoutExpression(KnockoutExpressionData data)
-    {
-      var sb = new StringBuilder();
-
-      sb.Append(Name);
-      sb.Append(" : {");
-      for (int i = 0; i < subItems.Count; i++)
-      {
-        if (i != 0)
-          sb.Append(",");
-        sb.Append(subItems[i].GetKnockoutExpression(data));
-      }
-      sb.Append('}');
-
-      return sb.ToString();
-    }
   }
 }
