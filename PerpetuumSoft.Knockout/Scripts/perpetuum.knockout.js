e62f6eea4d771a0b93fce87a58ba38af16d3a43a
Nigel Whatling <nigelw@n-vision.com.au>
2013-09-18 11:19:59 +1000

    Updated the script file.

--- PerpetuumSoft.Knockout/Scripts/perpetuum.knockout.js
+++ PerpetuumSoft.Knockout/Scripts/perpetuum.knockout.js
@@ -1,27 +1,70 @@
-﻿executeOnServer = function (model, url) {
+﻿executeOnServer = function (model, url, token, modelOut, executeSettings, successOverride) {
 
-    $.ajax({
+    var $this = $(this);
+
+    var settings = {
         url: url,
-        type: 'POST',
-        data: ko.mapping.toJSON(model),
+        type: (executeSettings && executeSettings.requestType) ? settings.requestType : 'POST',
+        data: (model) ? ko.mapping.toJSON(model) : null,
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function (data) {
-        	if (data.redirect) {
-        		location.href = resolveUrl(data.url);
-        	}
-        	else {
-        		ko.mapping.fromJS(data, model);
-        	}
+            if (data.redirect) {
+                location.href = resolveUrl(data.url);
+            }
+            else if (successOverride) {
+                successOverride(data);
+            }
+            else if (modelOut) {
+                ko.mapping.fromJS(data, modelOut);
+            }
+            else if (model) {
+                ko.mapping.fromJS(data, model);
+            }
+            if (executeSettings && executeSettings.success) {
+                executeSettings.success(jqXHR, settings);
+            }
         },
-        error: function (error) {
-            alert("There was an error posting the data to the server: " + error.responseText);
+        error: function (jqXHR, textStatus, errorThrown) {
+            var msg;
+            switch (textStatus) {
+                case "timeout":
+                    msg = "There was a timeout posting data to the server.";
+                    break;
+                case "abort":
+                    msg = "Posting data to the server was aborted.";
+                    break;
+                default:
+                    msg = "There was an error posting data to the server.";
+            }
+            if (errorThrown) {
+                msg = msg + "\r\n" + errorThrown;
+            }
+            alert(msg + "\r\n" + jqXHR.responseText);
+            if (executeSettings && executeSettings.error) {
+                executeSettings.error(jqXHR, settings);
+            }
+        },
+        beforeSend: function (jqXHR, settings) {
+            if (executeSettings && executeSettings.beforeSend) {
+                executeSettings.beforeSend(jqXHR, settings);
+            }
+        },
+        complete: function (jqXHR, textStatus) {
+            if (executeSettings && executeSettings.complete) {
+                executeSettings.complete(jqXHR, textStatus);
+            }
         }
-    });
+    };
 
+    if (token) {
+        settings.headers = { "X-Request-Verification-Token": token };
+    }
+
+    $.ajax(settings);
 };
 
- resolveUrl = function (url) {
+resolveUrl = function (url) {
     if (url.indexOf("~/") == 0) {
         url = baseUrl + url.substring(2);
     }
